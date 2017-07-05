1ed332212e752ddccaaf470cf6306cab4844b160
Rainbow: the Mad Faerie <rtmf@beautifulsunrise.org>
2017-07-04 19:15:51 -0700

    this should work!

--- proxy.c
+++ proxy.c
@@ -1,24 +1,33 @@
 #include <sys/select.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define BUFSIZE 16384
+#define BUFSIZE 128
 #define ARG_COUNT 4
 #ifndef MAX
 #define MAX(a,b) (a>b?a:b)
 #endif
+
 #define RD(ep) if (FD_ISSET(ep.socket,&rd)) ep_rd(&ep);
 #define WR(ep) if (FD_ISSET(ep.socket,&wr)) ep_wr(&ep);
-#define FD(ep) if (ep.to->bytes<BUFSIZE) FD_SET(ep.socket,&rd); if (ep.bytes>0) FD_SET(ep.socket,&wr);
+#define EX(ep) if (FD_ISSET(ep.socket,&ex)) {\
+	fprintf(stderr,"Exceptional condition on %s\n",ep.name); \
+	die("ep_ex",0);\
+}
+#define DO(ep) RD(ep) WR(ep) EX(ep)
+#define FD(ep) \
+	if (ep.to->bytes<BUFSIZE) FD_SET(ep.socket,&rd); \
+	if (ep.bytes>0) FD_SET(ep.socket,&wr); \
+	FD_SET(ep.socket,&ex);
 int die(char * why, int how)
 {
-	write(2,why,strlen(why));
-	write(2,"\n",strlen("\n"));
+	fprintf(stderr,"%s\n",why);
 	exit(how);
 }
 void usage()
@@ -45,8 +54,15 @@ void ep_rd(endpoint * ep)
 		fprintf(stderr,"Error reading from %s for %s.\n",ep->name,ep->to->name);
 		die("ep_rd",7);
 	}
+	if (ret==0)
+	{
+		fprintf(stderr,"Read 0 bytes from %s, link dead.\n",ep->name);
+		die("read0",0);
+	}
+#ifdef DEBUG
 	else
 		printf("Buffered %d bytes from %s for %s.\n",ret,ep->name,ep->to->name);
+#endif
 	ep->to->bytes+=ret;
 }
 void ep_wr(endpoint * ep)
@@ -58,8 +74,10 @@ void ep_wr(endpoint * ep)
 		fprintf(stderr,"Error writing to %s.\n",ep->name);
 		die("ep_rd",8);
 	}
+#ifdef DEBUG
 	else
 		printf("Wrote %d bytes to %s of %d buffered.\n",ret,ep->name,ep->bytes);
+#endif
 	ep->bytes-=ret;
 	memmove(ep->buffer,&(ep->buffer[ret]),ep->bytes);
 }
@@ -72,6 +90,7 @@ int main (int argc, char * argv[])
 	socklen_t size;
 	struct timeval tv;
 	int ret;
+	pid_t child;
 	if (argc != ARG_COUNT)
 		usage();
 	server.socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
@@ -87,12 +106,15 @@ int main (int argc, char * argv[])
 	listen(server.socket,3);
 	while (1)
 	{
+		child=waitpid(-1,&ret,WNOHANG);
+		if (child>0)
+			printf("Reaped child connection %d.\n",child);
 		size=sizeof(client.addr);
 		FD_ZERO(&rd);
 		FD_ZERO(&wr);
 		FD_ZERO(&ex);
 		FD_SET(server.socket,&rd);
-		tv.tv_sec=60;
+		tv.tv_sec=0;
 		tv.tv_usec=5;
 		ret=select(server.socket+1,&rd,&wr,&ex,&tv);
 		if (ret<0)
@@ -108,9 +130,10 @@ int main (int argc, char * argv[])
 			client.to=&remote;
 			if (client.socket<=0)
 				die("Accept failed.",4);
-			if (fork()==0)
+			if ((ret=fork()))
+				printf("[proxy] Forked client to child %d.\n",ret); 
+			else
 			{
-				printf("Connected client...\n");
 				remote.socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 				remote.addr.sin_family=AF_INET;
 				remote.addr.sin_addr.s_addr=inet_addr(argv[2]);
@@ -122,6 +145,7 @@ int main (int argc, char * argv[])
 				remote.to=&client;
 				if (connect(remote.socket,(struct sockaddr *)&remote.addr, sizeof(remote.addr))<0)
 					die("Could not connect to remote",5);
+				printf("[child] Client connected to remote.\n");
 				while(1)
 				{
 					FD_ZERO(&rd);
@@ -129,17 +153,15 @@ int main (int argc, char * argv[])
 					FD_ZERO(&ex);
 					FD(client);
 					FD(remote);
-					tv.tv_sec=60;
+					tv.tv_sec=0;
 					tv.tv_usec=5;
 					ret=select(MAX(client.socket,remote.socket)+1,&rd,&wr,&ex,&tv);
 					if (ret<0)
 						die("Client select failed.",6);
 					if (ret>0)
 					{	
-						RD(client);
-						RD(remote);
-						WR(client);
-						WR(remote);
+						DO(client);
+						DO(remote);
 					}
 				}
 			}
