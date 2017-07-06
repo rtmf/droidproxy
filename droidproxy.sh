27ec1e64be9d1144630cd86fc3804044b8736ae7
Rainbow: the Mad Faerie <rtmf@beautifulsunrise.org>
2017-07-05 22:03:12 -0700

    add SSH support for droidproxy infra, various

--- droidproxy.sh
+++ droidproxy.sh
@@ -1,28 +1,169 @@
 #!/usr/bin/zsh
+# vim: syntax=zsh ts=2 sw=2 noexpandtab
+#
+MODULE="droidproxy"
 PREFIX="/opt/droidproxy"
-REMOTE="74.207.244.165"
-REPORT="8080"
-ANPORT="5050"
-LOPORT="5050"
-BINARY="proxy"
-ANPATH="/data/local/tmp"
-DPROXY="${PREFIX}/${BINARY}"
-APROXY="${ANPATH}/${BINARY}"
-DOMAIN="$(dnsdomainname)"
+DOMAIN="$(hostname -d)"
 HOSTID="$(hostname -s)"
-adb kill-server
-rm -f /etc/resolv.conf
-cat > /etc/resolv.conf <<- __RESOLV_CONF__
-#resolv.conf, written by droidproxy
-nameserver 74.207.244.165
+CACERT="${PREFIX}/${DOMAIN}.pem"
+MYCERT="${PREFIX}/${HOSTID}.${DOMAIN}.crt"
+SECRET="${PREFIX}/${HOSTID}.${DOMAIN}.pem"
+THISIS="${PREFIX}/${MODULE}.sh"
+REMOTE="74.207.244.165"
+REPORT="1057"
+ANPORT="8008"
+LOPORT="8483"
+TUNTYP="ssh" 
+DNSSRV="${REMOTE}"
+DNSDOM="${DOMAIN}"
+SEARCH="tesayon.xh cccn.xh xh"
+VPNPID="${PREFIX}/openvpn.pid"
+TUNPID="${PREFIX}/proxy.pid"
+SVCPID="${PREFIX}/droidproxy.sh.pid"
+DAEMON={VPN,TUN,SVC}PID
+ANPATH="/data/local/tmp"
+RESOLV=$(<<-__RESOLV__
+# resolv.conf, written by ${MODULE}
+nameserver ${DNSSRV}
 domain ${DOMAIN}
-search ${DOMAIN} tesayon.xh cccn.xh xh
-__RESOLV_CONF__
-chmod 0755 ${DPROXY}
-adb push ${DPROXY} ${APROXY}
-adb shell "busybox killall -9 proxy"
-adb shell "${APROXY} ${ANPORT} ${REMOTE} ${REPORT} " &
-adb forward tcp:${LOPORT} tcp:${ANPORT}
-openvpn --config ${PREFIX}/${HOSTID}.${DOMAIN}.conf --remote 127.0.0.1 ${LOPORT}
-adb kill-server
-kill -9 %1
+search ${DOMAIN} ${SEARCH}
+__RESOLV__
+)
+case "${TUNTYP}" in
+	ssh)
+		SAPORT="7175"
+		SLPORT="2816"
+		BINARY="sshd"
+		TERMUX="/data/data/com.termux/files"
+		SSHDIS="${TERMUX}/usr/bin/${BINARY}"
+		SSHDLD="${TERMUX}/usr/lib"
+		SSHDPK="${PREFIX}/authorized_keys"
+		SSHDKF="${ANPATH}/authorized_keys"
+		SSHDCF="${ANPATH}/sshd_config"
+		SSHDOP="$(<<- __SSHD_CONFIG__
+										PasswordAuthentication			no
+										PubkeyAcceptedKeyTypes			ssh-rsa
+										Port												${SAPORT}
+										AuthorizedKeysFile					${SSHDKF}
+										StrictModes									no
+									__SSHD_CONFIG__
+								)"
+		SSHDGO="$(<<- __SSHD_STARTUP_SCRIPT__
+										#!/system/bin/sh
+										busybox killall -9 sshd
+										busybox chown \$(id -u):\$(id -g) ${SSHDKF} ${SSHDCF}
+										busybox chmod 0644 ${SSHDKF} ${SSHDCF}
+										export LD_LIBRARY_PATH=${SSHDLD}:\${LD_LIBRARY_PATH}
+										export PATH=${TERMUX}/usr/bin:${TERMUX}/usr/bin/applets:\${PATH}
+										${SSHDIS} -f ${SSHDCF} -p ${SAPORT} -d -D
+									__SSHD_STARTUP_SCRIPT__
+								)"
+								cat =(<<<"$SSHDGO")
+		SSHDSF="${ANPATH}/sshd.sh"
+		;;
+	droidproxy|dproxy|dp|droid|proxy)
+		BINARY="proxy"
+		DPROXY="${PREFIX}/${BINARY}"
+		APROXY="${ANPATH}/${BINARY}"
+		;;
+esac
+
+eval "${PIDINF}"
+ACTION="${1:-help}"
+case "${ACTION}" in
+	kill)
+		killall -9 openvpn
+		for RUNNER in $DAEMON
+		do
+			PFNAME="${(P)RUNNER}"
+			RUNPID="${$(<${PFNAME}):-0}" 2>/dev/null
+			(( ${RUNPID} )) && kill 9 ${RUNPID} && rm -f ${PFNAME}
+		done
+		;;
+	stop)
+		[ -f ${VPNPID} ] && kill -9 "$(<${VPNPID})"
+		sleep 0.5
+		[ -f ${SVCPID} ] && zsh "${THISIS}" kill
+		#this should be enough to get the internal teardown to happen
+		;;
+	start)
+		zsh "${THISIS}" kill
+		zsh "${THISIS}" fork &
+		echo "$!" > "${SVCPID}"
+		disown
+		;;
+	fork)
+		adb kill-server
+		rm -f /etc/resolv.conf
+		echo "${RESOLV}" > /etc/resolv.conf
+		adb shell "busybox killall -9 ${BINARY}"
+		case ${TUNTYP} in
+			droidproxy|dproxy|dp|droid|proxy)
+				adb forward tcp:${LOPORT} tcp:${ANPORT}
+				chmod 0755 ${DPROXY}
+				adb push ${DPROXY} ${APROXY}
+				(
+				adb shell "${APROXY} ${ANPORT} ${REMOTE} ${REPORT} " 
+				)&
+				;;
+			ssh)
+				ssh-keygen -R localhost
+				SSHTMP="$(mktemp -d -t droidproxy-ssh-tunnel-XXXX)"
+				[ -d "${SSHTMP}" ] || exit 255
+				ssh-keygen -t rsa -b 1024 -N "" -C "DroidProxy" -f ${SSHTMP}/key
+				SSHPUB="${SSHTMP}/key.pub"
+				SSHPRI="${SSHTMP}/key"
+				SSHDAK="$(<<- __SSHD_AUTHORIZED_KEYS__
+												$(<${SSHPUB})
+												${$(<${SSHDPK}):-}
+												$(ssh-add -L)
+											__SSHD_AUTHORIZED_KEYS__
+				)$" 2>/dev/null
+				echo "${SSHDAK}"
+				echo "${SSHDOP}"
+				adb push =(<<<${SSHDAK}) ${SSHDKF}
+				adb push =(<<<${SSHDOP}) ${SSHDCF}
+				adb forward tcp:${SLPORT} tcp:${SAPORT}
+				adb push =(<<<${SSHDGO}) ${SSHDSF}
+				adb shell "/system/xbin/busybox chmod 0755 ${SSHDSF}"
+				adb shell "su -c ${SSHDSF}" &
+				sleep 5
+				ssh-keyscan -vvv -p ${SLPORT} localhost | tee -a ${HOME}/.ssh/known_hosts
+				ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -N -oIdentityFile=${SSHPRI} -oIdentitiesOnly=yes -oIdentityAgent=none -oNoHostAuthenticationForLocalhost=yes -oPasswordAuthentication=no -oPreferredAuthentications=publickey -oPort=${SLPORT} -T -L ${LOPORT}:${REMOTE}:${REPORT} droidproxy@localhost & 
+				;;
+		esac
+		echo "$!" > ${TUNPID}
+		openvpn --client \
+			--remote localhost ${LOPORT} \
+			--proto tcp-client\
+			--ca ${CACERT} \
+			--key ${SECRET} \
+			--cert ${MYCERT} \
+			--comp-lzo yes \
+			--dev tun \
+			--persist-tun \
+			--persist-key \
+			--verb 3 \
+			--script-security 2 \
+			--tls-client \
+			--route 0.0.0.0 0.0.0.0 vpn_gateway \
+			--pull &
+		echo "$!" > ${VPNPID}
+		wait "$(<${TUNPID})"
+		adb kill-server
+		kill -9 "$(<${VPNPID})"
+		kill -9 "$(<${TUNPID})"
+		rm "${VPNPID}"
+		rm "${TUNPID}"
+		rm "${SVCPID}"
+		rm -rf "${SSHTMP}/"
+		exit 0
+		;;
+	restart)
+		zsh "${THISIS}" stop
+		zsh "${THISIS}" start
+		;;
+	help)
+		echo "I ran out of time."
+		;;
+esac
